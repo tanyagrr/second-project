3022186b604de8fb0f2f92bdee0af5893281a9a4
root <root@DESKTOP-A0MIIF9.>
2025-04-01 20:14:37 +0300

    add image

--- src/components/Info1.jsx
+++ src/components/Info1.jsx
@@ -18,10 +18,37 @@ function Info1() {
           </div>
           <button className="button copy-button">
             npm create sanity@latest
-            <span class="Button_icon__UBoOj Button_iconRight__IdvgI"></span>
+            <span className="copy">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#9499ad"
+                class="bi bi-copy"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                />
+              </svg>
+            </span>
           </button>
         </div>
-        <div className="image"></div>
+        <div className="image">
+          <img
+            alt=""
+            className="pic"
+            // width="990px"
+            // height="570px"
+            fetchpriority="high"
+            loading="eager"
+            decoding="async"
+            data-nimg="1"
+            srcset="https://cdn.sanity.io/images/3do82whm/next/a119c468f61ed6d8770a2f5aac5d41d9a3804394-3232x1868.png?w=3840&amp;q=75&amp;fit=clip&amp;auto=format 1x"
+            src="https://cdn.sanity.io/images/3do82whm/next/a119c468f61ed6d8770a2f5aac5d41d9a3804394-3232x1868.png?w=3840&amp;q=75&amp;fit=clip&amp;auto=format"
+          />
+        </div>
       </div>
     </>
   );
